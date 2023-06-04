5bb1c5b0ad62dadeecffc19131cad2d04cd92f93
RedVelvettz <135519461+RedVelvettz@users.noreply.github.com>
2023-06-04 16:02:47 -0500

    inicio

---
+++ Assets/Scripts/button.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class button : MonoBehaviour
+{
+   
+    public void CambiarNivel()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
