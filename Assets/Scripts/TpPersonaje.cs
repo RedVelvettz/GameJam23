7d80fb2ed0757d3921807af09aefe7c0017ab538
kevinqZz <36774434+kevinqZz@users.noreply.github.com>
2023-06-04 13:46:27 -0500

    teleports

--- Assets/Scripts/TpPersonaje.cs
+++ Assets/Scripts/TpPersonaje.cs
@@ -6,18 +6,36 @@ public class TpPersonaje : MonoBehaviour
 {
     private GameObject currentTeleport;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            if(currentTeleport !=null)
+            {
+                transform.position = currentTeleport.GetComponent<Telep>().GetDestination().position;
+            }
+        }
     }
-    public void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.CompareTag("Teleporter"))
+        {
+            Debug.Log("tpcolision");
+            currentTeleport = collision.gameObject;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            if(collision.gameObject == currentTeleport)
+            {
+                currentTeleport = null;
+            }
+           
+        }
     }
 }
