a98b69302608e31f92fdcbd0894bde3a5d8df64c
kevinqZz <36774434+kevinqZz@users.noreply.github.com>
2023-06-04 11:07:01 -0500

    Elevador

---
+++ Assets/Scripts/Elevador.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevador : MonoBehaviour
+{
+    public Transform player;
+    public Transform dowpos;
+    public Transform upperpos;
+    public Transform elevatorswitch;
+    public float speed;
+    bool iselevatordown;
+    public SpriteRenderer elevator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartElevator();
+    }
+    void StartElevator()
+    {
+        if (Vector2.Distance(player.position, elevatorswitch.position) < 0.5f && Input.GetKeyDown("f"))
+        {
+            if (transform.position.y <= dowpos.position.y)
+            {
+                iselevatordown = true;
+            }
+            else if (transform.position.y >= upperpos.position.y)
+            {
+                iselevatordown = false;
+            }
+
+        }
+        if (iselevatordown)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, upperpos.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, dowpos.position, speed * Time.deltaTime);
+        }
+
+    }
+
+    void DisplayColor()
+
+    {
+        if (transform.position.y <= dowpos.position.y || transform.position.y >= upperpos.position.y)
+        {
+            elevator.color = Color.green;
+        }
+        else
+        {
+            elevator.color = Color.red;
+        }
+    }
+}
