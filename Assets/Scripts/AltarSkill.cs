46bd9a68709b1c04fa0bf369fd6ff7fbbebf8f22
kevinqZz <36774434+kevinqZz@users.noreply.github.com>
2023-06-04 14:28:31 -0500

    estatuasdestruir

---
+++ Assets/Scripts/AltarSkill.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AltarSkill : MonoBehaviour
+{
+    int cont_skillr, cont_skillb, cont_skilly;
+    bool newSkillCounter;
+    void Start()
+    {
+        cont_skillr = 1;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("f") && newSkillCounter && (cont_skillr == 1))
+        {
+            //player dead
+            //carga de nuevo sprite
+            print("Nueva habilidad obtenida EMBESTIDA");
+            cont_skillr = 0;
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collisioner)
+    {
+        if ((collisioner.transform.tag == "Player") && (cont_skillr == 1))
+        {
+            print("aprender nueva habilidad?");
+            newSkillCounter = true;
+        }
+        else
+        {
+            newSkillCounter = false;
+        }
+    }
+}
--- Assets/Scripts/CorruptedEnergyBar.cs
+++ Assets/Scripts/CorruptedEnergyBar.cs
@@ -44,6 +44,6 @@ public class CorruptedEnergyBar : MonoBehaviour
                 }
             }
         }
-        print(corruptedBarImage.fillAmount);
+        //print(corruptedBarImage.fillAmount);
     }
 }
