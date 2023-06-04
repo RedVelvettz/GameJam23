70e43ca13fd36266a59619eb4fecdf9cebb400fe
kevinqZz <36774434+kevinqZz@users.noreply.github.com>
2023-06-04 12:14:58 -0500

    Elive

--- Assets/Scripts/Elevador.cs
+++ Assets/Scripts/Elevador.cs
@@ -4,51 +4,26 @@ using UnityEngine;
 
 public class Elevador : MonoBehaviour
 {
-    public Transform player;
-    public Transform dowpos;
-    public Transform upperpos;
-    public Transform elevatorswitch;
-
-    public float speed;
-    bool iselevatordown;
-   
-
-
-    // Start is called before the first frame update
-    void Start()
+    float changer;
+    private void Start()
     {
+        changer = 1f;
+    }
+    private void Update()
+    {
+        gameObject.transform.Translate(0, changer * Time.deltaTime, 0);
+    }
+    private void OnTriggerEnter2D(Collider2D collisioner)
+    {
+        if (collisioner.gameObject.tag == "starteleve")
+        {
+            changer = 10f;
+        }
+        if (collisioner.gameObject.tag == "finaleleve")
+        {
+            changer = -10f;
+        }
 
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            Debug.Log("sube");
-            if (transform.position.y <= dowpos.position.y)
-            {
-                iselevatordown = true;
-            }
-            else if (transform.position.y >= upperpos.position.y)
-            {
-                iselevatordown = false;
-            }
-
-        }
-        if (iselevatordown)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, upperpos.position, speed);
-        }
-        else
-        {
-            transform.position = Vector2.MoveTowards(transform.position, dowpos.position, speed);
-        }
-    }
-
 
 }
